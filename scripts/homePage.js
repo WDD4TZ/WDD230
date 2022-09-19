068945ab9f9f80f6c4c5bf919adfb372bcc8701d
WDD4TZ <zan19004@byui.edu>
2022-09-19 17:10:55 -0600

    fixed homepage issues

---
+++ scripts/homePage.js
@@ -0,0 +1,5 @@
+/*<script>
+    const d = new Date();
+    const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    document.getElementById("date").innerHTML = d;
+</script>*/
